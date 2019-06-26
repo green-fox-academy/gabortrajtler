5202bc2a8d548967520e9894bf02bbac0de09d21
Gábor Trajtler <trajtlerg@gmail.com>
2019-06-26 14:16:58 +0200

    Add project: test (week 1)

---
+++ week-01/day-3/test/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    std::string a = "hello";
+    std::cout << a << "\n";
+
+    bool boolean = 566-565; // 0 is false, 1 (& anything) is 1 = true
+    std::cout << "b:" << boolean << "\n";
+
+    int integer;
+    std::cout << integer;
+
+    return 0;
+}
+
