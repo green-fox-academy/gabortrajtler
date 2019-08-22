d75b5349f3a66021d4d1ae2a11b1f609ce507434
Gábor Trajtler <trajtlerg@gmail.com>
2019-08-22 14:42:02 +0200

    Remove unused imports from "Listing Todos H2" exercise

--- week-08/day-4/todos_embedded_h2_db/src/main/java/com/greenfox/tgabor/todos_embedded_h2_db/controllers/TodoController.java
+++ week-08/day-4/todos_embedded_h2_db/src/main/java/com/greenfox/tgabor/todos_embedded_h2_db/controllers/TodoController.java
@@ -6,11 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class TodoController {
---
+++ week-08/day-4/todos_mysql/src/main/java/com/greenfox/tgabor/todos_mysql/TodosMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.tgabor.todos_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodosMysqlApplication.class, args);
+  }
+
+}
