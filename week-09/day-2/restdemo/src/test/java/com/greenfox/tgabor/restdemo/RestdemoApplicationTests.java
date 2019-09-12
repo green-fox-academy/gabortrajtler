abcf967a11430fbab7417a5109bba69c6357e1e5
Gábor Trajtler <trajtlerg@gmail.com>
2019-09-12 15:56:52 +0200

    Do proper mocking of services

--- week-09/day-2/restdemo/src/test/java/com/greenfox/tgabor/restdemo/RestdemoApplicationTests.java
+++ week-09/day-2/restdemo/src/test/java/com/greenfox/tgabor/restdemo/RestdemoApplicationTests.java
@@ -1,12 +1,18 @@
 package com.greenfox.tgabor.restdemo;
 
+import com.greenfox.tgabor.restdemo.controllers.DoUntilController;
 import com.greenfox.tgabor.restdemo.controllers.GreeterController;
+import com.greenfox.tgabor.restdemo.models.Greeter;
+import com.greenfox.tgabor.restdemo.services.ArrayService;
 import com.greenfox.tgabor.restdemo.services.DoUntilService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,11 +20,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(GreeterController.class)
+@WebMvcTest  // @WebMvcTest(GreeterController.class) -  if you wan't to test only selected contoller. Other @Tests will give 404.
 public class RestdemoApplicationTests {
 
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
@@ -55,16 +62,19 @@ public class RestdemoApplicationTests {
   }
 
   @MockBean
-  DoUntilService doUntilService;
+  DoUntilService doUntilService;  // somehow cannot count, must set result with given()
+  @MockBean
+  ArrayService arrayService;
 
   @Test
   public void dountil_successfull() throws Exception {
+    given(this.doUntilService.sum(5)).willReturn(15);
     mockMvc.perform(post("/dountil/sum")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"until\": \"5\"}")
     ).andExpect(status().isOk())
             .andExpect(content().contentType(contentType))
-            .andExpect(jsonPath("$.until", is("5")))
-            .andExpect(jsonPath("$.result", is("15")));
+            .andExpect(jsonPath("$.until", is(5)))
+            .andExpect(jsonPath("$.result", is(15)));
   }
 }
