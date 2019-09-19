cf199e8dae65171ea06d7142e85ee36ca3f98553
Gábor Trajtler <trajtlerg@gmail.com>
2019-09-19 18:02:46 +0200

    Add test data at init

--- week-11/day-4/TodoApp/TodoApp/ApplicationContext.cs
+++ week-11/day-4/TodoApp/TodoApp/ApplicationContext.cs
@@ -15,6 +15,51 @@ namespace TodoApp
 
         public DbSet<Todo> Todos { get; set; }
         public DbSet<Assignee> Assignees { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
+            #region Assignees for test
+            var assignee1 = new Assignee()
+            {
+                AssigneeId = 1,
+                Email = "red.fox.tester@greenfox.hu",
+                Name = "Red Fox"
+            };
+
+            var assignee2 = new Assignee()
+            {
+                AssigneeId = 2,
+                Email = "blue.fox.tester@greenfox.hu",
+                Name = "Blue Fox"
+            };
+
+            builder.Entity<Assignee>().HasData(assignee1);
+            builder.Entity<Assignee>().HasData(assignee2);
+            #endregion
+
+            #region Todos for test
+            var todo1 = new Todo()
+            {
+                AssigneeId = 1,
+                Description = "You can create new Todos in the menu!",
+                Title = "Create your first Todo!",
+                TodoId = 1,
+                Done = false
+            };
+
+            var todo2 = new Todo()
+            {
+                AssigneeId = 1,
+                Description = "Use the built-in OnGet() method of the Razor page",
+                Title = "Refactor Assignee select-list when adding Todo",
+                TodoId = 2,
+                Done = false
+            };
+
+            builder.Entity<Todo>().HasData(todo1);
+            builder.Entity<Todo>().HasData(todo2);
+            #endregion
+        }
     }
 }
---
+++ week-11/day-4/TodoApp/TodoApp/Migrations/20190919160049_add-test-data-1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApp.Migrations
+{
+    public partial class addtestdata1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Assignees",
+                columns: new[] { "AssigneeId", "Email", "Name" },
+                values: new object[] { 1L, "red.fox.tester@greenfox.hu", "Red Fox" });
+
+            migrationBuilder.InsertData(
+                table: "Assignees",
+                columns: new[] { "AssigneeId", "Email", "Name" },
+                values: new object[] { 2L, "blue.fox.tester@greenfox.hu", "Blue Fox" });
+
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "TodoId", "AssigneeId", "Description", "Done", "Title" },
+                values: new object[] { 1L, 1L, "You can create new Todos in the menu!", false, "Create your first Todo!" });
+
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "TodoId", "AssigneeId", "Description", "Done", "Title" },
+                values: new object[] { 2L, 1L, "Use the built-in OnGet() method of the Razor page", false, "Refactor Assignee select-list when adding Todo" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "AssigneeId",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "TodoId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "TodoId",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Assignees",
+                keyColumn: "AssigneeId",
+                keyValue: 1L);
+        }
+    }
+}
